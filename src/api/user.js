d03b6a6a3fd6b3cf801986f8e3008e8f505b5fdb
dean <dean@airdb.com>
2019-01-29 09:38:49 +0800

    update

--- src/api/user.js
+++ src/api/user.js
@@ -6,7 +6,7 @@ export const login = ({ userName, password }) => {
     password
   }
   return axios.request({
-    url: 'login',
+    url: '/wechatapi/small/user/login',
     data,
     method: 'post'
   })
@@ -15,7 +15,7 @@ export const login = ({ userName, password }) => {
 // Web login thought Weixin QR code.
 export const wxlogin = ({ code }) => {
   return axios.request({
-    url: 'user/login',
+    url: '/wechatapi/small/user/login',
     params: {
       code
     },
@@ -35,61 +35,7 @@ export const getUserInfo = (token) => {
 
 export const logout = (token) => {
   return axios.request({
-    url: 'user/logout',
+    url: '/wechatapi/small/user/logout',
     method: 'post'
-  })
-}
-
-export const getUnreadCount = () => {
-  return axios.request({
-    url: 'message/count',
-    method: 'get'
-  })
-}
-
-export const getMessage = () => {
-  return axios.request({
-    url: 'message/init',
-    method: 'get'
-  })
-}
-
-export const getContentByMsgId = msg_id => {
-  return axios.request({
-    url: 'message/content',
-    method: 'get',
-    params: {
-      msg_id
-    }
-  })
-}
-
-export const hasRead = msg_id => {
-  return axios.request({
-    url: 'message/has_read',
-    method: 'post',
-    data: {
-      msg_id
-    }
-  })
-}
-
-export const removeReaded = msg_id => {
-  return axios.request({
-    url: 'message/remove_readed',
-    method: 'post',
-    data: {
-      msg_id
-    }
-  })
-}
-
-export const restoreTrash = msg_id => {
-  return axios.request({
-    url: 'message/restore',
-    method: 'post',
-    data: {
-      msg_id
-    }
   })
 }
