99dfdaa88719e7bd4688707d3575b1375e274166
YiNo <dean@airdb.com>
2019-04-09 11:45:58 +0800

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,14 +3,14 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
       <router-link to="/" class="navbar-brand">{{website}}</router-link>
-      
+
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item">
             <router-link to="/forum" class="nav-link"> 论坛 </router-link>
           </li>
           <li class="nav-item">
-            <a href="/moments" class="nav-link"> 圈子 </a>
+            <a href="/chatroom" class="nav-link"> 圈子 </a>
           </li>
           <li class="nav-item">
             <router-link to="/admin" class="nav-link"> 后台管理 </router-link>
@@ -42,8 +42,8 @@
       <router-link to="/login" class="btn btn-primary btn-md rounded-0" style="font-size: 13px"> 登陆 </router-link>
     </div>
     <!-- login end -->
-    
-  </nav>  
+
+  </nav>
   <!-- navbar end -->
 </div>
 </template>
