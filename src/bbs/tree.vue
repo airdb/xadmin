edd5eb504861022964adafba712aba47f3c63ed6
ye cai <yushunchn@gmail.com>
2020-08-08 18:37:34 +0800

    update bbs/tree

--- src/bbs/tree.vue
+++ src/bbs/tree.vue
@@ -1,77 +1,84 @@
 <template>
-    <div>
-        <NvTree
-        :items="items"
-        :search="search"
-        :checkbox="checkbox"
-        @on-check-change="onCheckChangeHandler"
-        :width="300"
-        >
-        </NvTree>
-    </div>
+  <div class="treeWrapper">
+    <NvTree
+      :items="items"
+      :search="search"
+      :checkbox="checkbox"
+      :width="50"
+      :editIcon="editIcon"
+      @on-check-change="onCheckChangeHandler"
+    ></NvTree>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'nvTreeCheckedDemo',
-    data() {
-        const self = this;
-        return {
-            search: true,
-            checkbox: true,
-            items: [
-                {
-                    title: 'node1',
-                    name: 'node1',
-                    icon: 'star',
-                    children: [
-                        {
-                            title: 'node1-2',
-                            name: 'node1-2',
-                            icon: 'star'
-                        },
-                        {
-                            title: 'node1-3',
-                            name: 'node1-3',
-                            icon: 'star'
-                        }
-                    ]
-                },
-                {
-                    title: 'node2',
-                    name: 'node2',
-                    icon: 'star',
-                    children: [
-                        {
-                            title: 'node2-1',
-                            name: 'node2-1',
-                            icon: 'star'
-                        },
-                        {
-                            title: 'node2-2',
-                            name: 'node2-2',
-                            icon: 'star'
-                        }
-                    ]
+import { mapActions, mapState } from "vuex";
 
-                },
-                {
-                    title: 'node3',
-                    name: 'node3',
-                    icon: 'star'
-                }
-            ]
-        };
+export default {
+  name: "nvTreeCheckedDemo",
+  computed: mapState({ items: "treeItems" }),
+  data() {
+    const self = this;
+    return {
+      search: true,
+      checkbox: true
+      //   items: [
+      //     {
+      //       title: "node1",
+      //       name: "node1",
+      //       icon: "pie",
+      //       children: [
+      //         {
+      //           title: "node1-2",
+      //           name: "node1-2",
+      //           icon: "heart"
+      //         },
+      //         {
+      //           title: "node1-3",
+      //           name: "node1-3",
+      //           icon: "star"
+      //         }
+      //       ]
+      //     },
+      //     {
+      //       title: "node2",
+      //       name: "node2",
+      //       icon: "star",
+      //       children: [
+      //         {
+      //           title: "node2-1",
+      //           name: "node2-1",
+      //           icon: "star"
+      //         },
+      //         {
+      //           title: "node2-2",
+      //           name: "node2-2",
+      //           icon: "star"
+      //         }
+      //       ]
+      //     },
+      //     {
+      //       title: "node3",
+      //       name: "node3",
+      //       icon: "star"
+      //     }
+      //   ]
+    };
+  },
+  methods: {
+    ...mapActions(["getTreeItems"]),
+    onCheckChangeHandler: function() {
+      this.$Message.info("check changed");
     },
-    methods: {
-        onCheckChangeHandler: function () {
-            this.$Message.info('check changed');
-        }
+    initTree() {
+      this.$store.dispatch("getTreeItems");
     }
+  },
+  mounted() {
+    this.initTree();
+  }
 };
 </script>
 
-<style lang="less">
+<style src="./tree.less" lang="less">
 </style>
-
-
