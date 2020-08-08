25eaebc0fbe72dcf40eee3db6262c04f07f6bd65
deancn <dean@airdb.com>
2020-08-09 01:25:31 +0800

    add mockup

--- src/bbs/tree.vue
+++ src/bbs/tree.vue
@@ -22,47 +22,7 @@ export default {
     return {
       search: true,
       checkbox: true
-      //   items: [
-      //     {
-      //       title: "node1",
-      //       name: "node1",
-      //       icon: "pie",
-      //       children: [
-      //         {
-      //           title: "node1-2",
-      //           name: "node1-2",
-      //           icon: "heart"
-      //         },
-      //         {
-      //           title: "node1-3",
-      //           name: "node1-3",
-      //           icon: "star"
-      //         }
-      //       ]
-      //     },
-      //     {
-      //       title: "node2",
-      //       name: "node2",
-      //       icon: "star",
-      //       children: [
-      //         {
-      //           title: "node2-1",
-      //           name: "node2-1",
-      //           icon: "star"
-      //         },
-      //         {
-      //           title: "node2-2",
-      //           name: "node2-2",
-      //           icon: "star"
-      //         }
-      //       ]
-      //     },
-      //     {
-      //       title: "node3",
-      //       name: "node3",
-      //       icon: "star"
-      //     }
-      //   ]
+      // items: []  // refer: mockup/api/tree/default.js
     };
   },
   methods: {
