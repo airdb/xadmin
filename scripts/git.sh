df68aab180f5882760adada5e0f35922fbc72705
deancn <dean@airdb.com>
2022-06-05 01:50:16 +0800

    modify git tag

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,7 +2,8 @@
 branch=`git symbolic-ref --short -q HEAD`
 
 # 获取所有分支上最新 tag
-tag=`git describe --tags $(git rev-list --tags --max-count=1)`
+#tag=`git describe --tags $(git rev-list --tags --max-count=1)`
+tag=`git describe --dirty --always --tags`
 
 cat > src/version.js <<EOF
 export const branch = '$branch'
